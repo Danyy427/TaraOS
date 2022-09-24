91bae91117be37d711ac6a98e6c24d1f311d8efc
Ats <isfendiyar510@gmail.com>
2022-09-25 00:57:58 +0300

    Trying stuff
    idk man 💀

--- TaraOSv1/src/boot/bios/include/printhex.asm
+++ TaraOSv1/src/boot/bios/include/printhex.asm
@@ -2,27 +2,9 @@
 
 ; params: si -> hex number to be printed
 printhex:
-    mov ah, 0x0e
-    xor al, al
-
-findlen:
-    xor cx, cx
-
-findlenloop:
-    inc cx
-    cmp [si+cx], 0x0
-    je printhexloop
-    jmp findlenloop
-
-printhexloop:
-    mov al, byte[si+cx]
-    add al, 0x30 ; convert to char
-    int 0x10
-
-    dec cx
-    cmp cx, 0
-    je endprinthex
-    jmp printhexloop
+    mov ax, si
+    xor bx, bx
+    
 
 endprinthex:
     ret
