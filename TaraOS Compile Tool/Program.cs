4010b362f75f6972af5ffba15183a3e1b48c447d
Danyy427 <ozgur.guzeldereli@gmail.com>
2022-09-25 02:21:02 +0300

    Added some more fixes

--- TaraOS Compile Tool/Program.cs
+++ TaraOS Compile Tool/Program.cs
@@ -3,80 +3,88 @@ using System.IO;
 using System.Reflection;
 using System.Text.RegularExpressions;
 
-string endFileName = "TaraOS.img";
-
-string compiler = "gcc";
-string assembler = "nasm";
-string linker = "ld";
-string copyTool = "copy";
-string emulator = "qemu-system-x86_64";
-
-string assemblerFlagsBin = "-fbin";
-
-string sourceFolder = ".\\src";
-string outputBin = ".\\bin";
-string outputArtifacts = ".\\artifacts";
-
-string[] assemblyBinFolders = { Path.Combine(sourceFolder, "boot\\bios\\boot") };
-string assemblyIncFolder = Path.Combine(sourceFolder, "boot\\bios\\include");
-string[] copyCatFileOrder = { "mbr.bin" };
-
-Console.WriteLine($"> Compiling Tara OS...");
-Console.WriteLine($"> Preffered Assembler is: {assembler}");
-
-clear();
-compileAssembly(assemblyBinFolders);
-copyTogether();
-run();
-
-Console.ReadLine();
-
-void clear()
+namespace TaraOS_Compile_Tool
 {
-	foreach (var file in new DirectoryInfo(outputArtifacts).GetFiles())
-	{
-		file.Delete();
-	}
-    foreach (var file in new DirectoryInfo(outputBin).GetFiles())
+    public partial class TaraOSCTool
     {
-        file.Delete();
+        static string endFileName = "TaraOS.img";
+
+        static string compiler = "gcc";
+        static string assembler = "nasm";
+        static string linker = "ld";
+        static string copyTool = "copy";
+        static string emulator = "qemu-system-x86_64";
+
+        static string assemblerFlagsBin = "-fbin";
+
+        static string sourceFolder = ".\\src";
+        static string outputBin = ".\\bin";
+        static string outputArtifacts = ".\\artifacts";
+
+        static string[] assemblyBinFolders = { Path.Combine(sourceFolder, "boot\\bios\\boot") };
+        static string assemblyIncFolder = Path.Combine(sourceFolder, "boot\\bios\\include");
+        static string[] copyCatFileOrder = { "mbr.bin" };
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"> Compiling Tara OS...");
+            Console.WriteLine($"> Preffered Assembler is: {assembler}");
+
+            clear();
+            compileAssembly(assemblyBinFolders);
+            copyTogether();
+            run();
+
+            Console.ReadLine();
+
+        }
+
+
+        static void clear()
+        {
+            foreach (var file in new DirectoryInfo(outputArtifacts).GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (var file in new DirectoryInfo(outputBin).GetFiles())
+            {
+                file.Delete();
+            }
+        }
+
+
+
+        static void compileAssembly(string[] assemblyBinFolers)
+        {
+            foreach (var folder in assemblyBinFolders)
+            {
+                var assemblyFiles = new DirectoryInfo(folder).GetFiles();
+
+                foreach (var assemblyFile in assemblyFiles)
+                {
+                    shellRun($"{assembler} {assemblerFlagsBin} {assemblyFile} -i {assemblyIncFolder} -o {Path.Combine(outputArtifacts, Path.GetFileNameWithoutExtension(assemblyFile.FullName) + ".bin")}");
+                }
+            }
+        }
+
+        static void copyTogether()
+        {
+            string query = $"{copyTool} /b ";
+
+            foreach (var file in copyCatFileOrder)
+            {
+                query += $"\"{Path.Combine(outputArtifacts, file)}\" + ";
+            }
+            query = query.Substring(0, query.Length - 2) + "/b \"" + Path.Combine(outputBin, endFileName) + "\"";
+
+            Console.WriteLine(query);
+            shellRun(query);
+        }
+
+        static void run()
+        {
+            shellRun(emulator + $" -monitor stdio {Path.Combine(outputBin, endFileName)}");
+        }
     }
 }
 
-void shellRun(string q)
-{
-    var process = System.Diagnostics.Process.Start("CMD.exe", $"/C {q}");
-	process.WaitForExit();
-}
-
-void compileAssembly(string[] assemblyBinFolers)
-{
-	foreach (var folder in assemblyBinFolders)
-	{
-		var assemblyFiles = new DirectoryInfo(folder).GetFiles();
-
-		foreach (var assemblyFile in assemblyFiles)
-		{
-			shellRun($"{assembler} {assemblerFlagsBin} {assemblyFile} -i {assemblyIncFolder} -o {Path.Combine(outputArtifacts, Path.GetFileNameWithoutExtension(assemblyFile.FullName) + ".bin")}");
-		}
-	}
-}
-
-void copyTogether()
-{
-	string query = $"{copyTool} /b ";
-
-	foreach (var file in copyCatFileOrder)
-	{
-		query += $"\"{Path.Combine(outputArtifacts, file)}\" + ";
-	}
-	query = query.Substring(0, query.Length - 2) + "/b \"" + Path.Combine(outputBin, endFileName) + "\"";
-
-    Console.WriteLine(query);
-	shellRun(query);
-}
-
-void run()
-{
-	shellRun(emulator + $" -monitor stdio {Path.Combine(outputBin, endFileName)}");
-}
---
+++ TaraOS Compile Tool/ShellRun.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaraOS_Compile_Tool
+{
+    public partial class TaraOSCTool
+    {
+#if __MACOS__
+        static void shellRun(string q)
+        {
+            // according to: https://stackoverflow.com/a/15262019/637142
+            // thans to this we will pass everything as one command
+            q = q.Replace("\"","\"\"");
+
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = "-c \""+ q + "\"",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+            proc.WaitForExit();
+
+            return proc.StandardOutput.ReadToEnd();
+        }
+#else 
+        static void shellRun(string q)
+        {
+            var process = System.Diagnostics.Process.Start("CMD.exe", $"/C {q}");
+            process.WaitForExit();
+        }
+#endif
+    }
+}
