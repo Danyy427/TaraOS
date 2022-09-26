c6f8027cc6f937181ed1dc573d48f4db56ce6696
Ats <isfendiyar510@gmail.com>
2022-09-26 12:14:20 +0300

    AUGHHHH
    probably segfaults

--- TaraOSv1/src/boot/bios/include/printhex.asm
+++ TaraOSv1/src/boot/bios/include/printhex.asm
@@ -7,13 +7,45 @@ printhex:
 
 reverseloop:
     ; ax = divisor * al + ah
+    ; divide by 16 to get the first digit
     mov ax, ah
-    div 10
+    div 16
 
+    ; multiply the remainder by 16 to reverse it
     mov ax, ah
-    mul 10
-    add ax, ah
+    mul 16
+
+    ; store the reversed number in si
+    mov si, ax
+    add si, ah
+
+    ; loop logic
+    inc word[counter]
+    cmp word[counter], 4
+    je endrevloop
+    jmp reverseloop
+
+endrevloop:
+    xor word[counter], word[counter]
+
+hexprintloop:
+
+    mov ax, si
+    div 16
     
+    mov si, al ; si/=16
+    mov al, [ah]
+    mov ah, 0x0e
+
+    int 0x10
+
+    ; loop logic
+    inc word[counter]
+    cmp word[counter], 4
+    je endprinthex
+    jmp hexprintloop
 
 endprinthex:
     ret
+
+counter dw 0
