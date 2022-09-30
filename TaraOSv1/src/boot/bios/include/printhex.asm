4330ae1454582a7dffaf6be42e7e13625819e3a5
Ats <isfendiyar510@gmail.com>
2022-09-30 11:30:57 +0300

    Stack magic

--- TaraOSv1/src/boot/bios/include/printhex.asm
+++ TaraOSv1/src/boot/bios/include/printhex.asm
@@ -1,51 +1,43 @@
 [BITS 16]
 
-; params: si -> hex number to be printed
-printhex:
-    xor bx, bx
-    mov ah, si
+; Print Hex Number
+; input: dx
 
-reverseloop:
-    ; ax = divisor * al + ah
-    ; divide by 16 to get the first digit
-    mov ax, ah
-    div 16
+hexsetup:
+    push cx
+    push bx
+    xor cx, cx
 
-    ; multiply the remainder by 16 to reverse it
-    mov ax, ah
-    mul 16
+hexreverse:
+    push ax
 
-    ; store the reversed number in si
-    mov si, ax
-    add si, ah
+    mov ax, 4
+    mul cx
 
-    ; loop logic
-    inc word[counter]
-    cmp word[counter], 4
-    je endrevloop
-    jmp reverseloop
-
-endrevloop:
-    xor word[counter], word[counter]
-
-hexprintloop:
-
-    mov ax, si
-    div 16
+    push cx
+    mov cx, ax
+    shr dx, cl
+    pop cx
     
-    mov si, al ; si/=16
-    mov al, [ah]
-    mov ah, 0x0e
+    and dx, 0x000F
 
+    mov bx, hexNums
+    add bx, dx
+    mov al, [bx]
+    mov ah, 0x0e
     int 0x10
 
-    ; loop logic
-    inc word[counter]
-    cmp word[counter], 4
-    je endprinthex
-    jmp hexprintloop
+    pop ax
 
-endprinthex:
+    inc cx
+
+    cmp cx, 4 
+    je endreverse
+    jmp hexreverse
+
+endreverse:
+    pop bx
+    pop cx
     ret
 
-counter dw 0
+hexNums: db "0123456789abcdef"
